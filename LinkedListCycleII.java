d925d933030732db85a6b67cb1f0256e035b57c5
Harsha-Reddy <43820092+HarshaReddyVardhan@users.noreply.github.com>
2025-06-05 23:36:26 -0500

    Update LinkedListCycleII.java

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -1,1 +1,40 @@
+// Time: O(n)
 
+// Space: O(1) (no extra data structure)
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while (fast != null && fast.next != null){
+            slow  = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            return null;
+        slow = head;
+        while(slow != fast){
+            slow  = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+}
